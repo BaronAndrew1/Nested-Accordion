adeb542ccb4e1f586006c5b5affb5d954741ce59
Andrew Baron <Drowelfofdead@gmail.com>
2016-02-16 13:29:56 -0500

    add jquery code file and edit html and css as necessary

---
+++ assets/js/jquery-version.js
@@ -0,0 +1,15 @@
+$("#clicks1").click(function() {
+  $(".internal-text1").toggleClass("showing");
+  $("#arrow1").toggleClass("fa fa-arrow-right blue");
+  $("#arrow1").toggleClass("fa fa-arrow-down");
+});
+$("#clicks2").click(function() {
+  $(".internal-text2").toggleClass("showing");
+  $("#arrow2").toggleClass("fa fa-arrow-right blue");
+  $("#arrow2").toggleClass("fa fa-arrow-down");
+});
+$("#clicks3").click(function() {
+  $(".internal-text3").toggleClass("showing");
+  $("#arrow3").toggleClass("fa fa-arrow-right blue");
+  $("#arrow3").toggleClass("fa fa-arrow-down");
+});
